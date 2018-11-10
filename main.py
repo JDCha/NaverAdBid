72495782e9ec120e4a7d7f263742405acaed1d5e
TaeGyeongLee <taegyeong7202@gmail.com>
2018-11-10 22:21:50 +0900

    입찰금액 조정

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import time
 
 browser = webdriver.Chrome("/Users/itaegyeong/PycharmProjects/NaverAd/data/chromedriver")
 
+
 def load_data_file():
     wb = load_workbook('/Users/itaegyeong/PycharmProjects/NaverAd/data/data.xlsx')
     sheet1 = wb['Sheet']
@@ -14,8 +15,9 @@ def load_data_file():
     dict_data_list = list()
 
     for index, item in enumerate(data_list):
-        if index==0:
+        if index == 0:
             continue
+
         data_dict = {}
         data_dict['group_id'] = item[0].value
         data_dict['group_name'] = item[1].value
@@ -27,11 +29,20 @@ def load_data_file():
         data_dict['keyword_name'] = item[7].value
         data_dict['current_bid'] = item[8].value
         data_dict['hope_rank'] = item[9].value
-        data_dict['addition'] = item[10].value
-        data_dict['max_bix'] = item[11].value
+        data_dict['plus_minus_money'] = item[10].value
+        data_dict['current_rank'] = item[11].value
+        data_dict['pc_ad_count'] = item[12].value
+        data_dict['mobile_ad_count'] = item[12].value
+        data_dict['domain'] = item[13].value
+
         dict_data_list.append(data_dict)
 
     return dict_data_list
+
+
+def return_html(browser_page_source):
+    html = BeautifulSoup(browser_page_source, "html.parser")
+    return html
 
 
 def naver_login(id, pw, dict_data_list):
@@ -45,11 +56,11 @@ def naver_login(id, pw, dict_data_list):
     browser.find_element_by_xpath('//*[@id="container"]/my-screen/div/div[1]/div/my-screen-board/div/div[1]/ul/li[1]/a').click()
     time.sleep(5)
 
-    # 가감액 체크,
     for item in dict_data_list:
         keyword_id = item['keyword_id']
 
         browser.switch_to.window(browser.window_handles[1])
+        time.sleep(3)
 
         browser.find_element_by_xpath(
             '//*[@id="wrap"]/div/div/div[1]/div[2]/div/div[2]/div/div/div/form/div/input').send_keys(keyword_id)
@@ -61,29 +72,51 @@ def naver_login(id, pw, dict_data_list):
         time.sleep(10)
         browser.implicitly_wait(1000)
 
+        # 노출 현황보기 클릭
         browser.execute_script("document.querySelector('#wgt-{keyword} > td:nth-child(10) > a').click();".format(keyword=keyword_id))
         browser.find_element_by_xpath('//*[@id="wrap"]/div[1]/div/div/div/div[2]/div[2]/div[3]')
 
-        html = BeautifulSoup(browser.page_source, "html.parser")
+        html = return_html(browser.page_source)
         rank_html = html.find('div',{"class":"scroll-wrap"})
 
-        print(len(rank_html.find_all("div",{"class":"content ng-scope"})))
+        pc_rank_list = rank_html.find_all("div",{"class":"content ng-scope"})
+        item['pc_ad_count'] = len(pc_rank_list)
+
+        # 현재 광고 순위 체크
+        for i, pc_rank in enumerate(pc_rank_list):
+            if item['pc_url'] == pc_rank.find('a',{'class':'lnk_tit ng-binding ng-scope'})['href']:
+                item['current_rank'] = i
 
         time.sleep(5)
 
+        # 모바일 칸으로 이동 및 모바일 광고 개수 크롤링
         browser.find_element_by_xpath('//*[@id="wrap"]/div[1]/div/div/div/div[2]/div[1]/ul/li[2]/a').click()
+        html = return_html(browser.page_source)
+        mobile_rank_html = html.find('div', {"class": "scroll-wrap"})
+        item['mobile_ad_count'] = len(mobile_rank_html.find_all("div", {"class": "content ng-scope"}))
 
-        html = BeautifulSoup(browser.page_source, "html.parser")
-        rank_html = html.find('div', {"class": "scroll-wrap"})
+        # 닫기 버튼 눌리기
+        browser.find_element_by_xpath('//*[@id="wrap"]/div[1]/div/div/div/div[1]/div[1]/button/i').click()
 
-        print(len(rank_html.find_all("div", {"class": "content ng-scope"})))
+        # 순위체크
+        new_bid = item['current_bid']
+        if item['current_rank'] < item['hope_rank']:
+            new_bid = new_bid - item['plus_minus_money']
+        elif item['current_rank'] > item['hope_rank']:
+            new_bid = new_bid + item['plus_minus_money']
 
+        # 입찰 금액 변경
+        browser.execute_script(
+            "document.querySelector('#wgt-{keyword} > td.cell-bid-amt.text-right.txt-r > a').click();".format(keyword=keyword_id))
 
+        bid_input_box = browser.find_element_by_xpath('//*[@id="wgt-{keyword}"]/td[5]/a/div/div/div[2]/div[1]/div/span/input'.format(keyword=keyword_id)).clear()
+        bid_input_box.send_keys(new_bid)
+        item['current_rank'] = new_bid
+
+        print(item)
 
         break
 
-
-    #browser.find_element_by_xpath('//*[@id="wgt-{keyword}"]/td[10]/a'.format(keyword=keyword_id)).click()
 
 if __name__ == '__main__':
     id = 'tourtopping'
